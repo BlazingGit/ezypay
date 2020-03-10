5b0d79b5539147e1026aa279531e4de9bdb84a23
BlazingGit <cw_yang@live.com>
2020-03-10 15:12:59 +0800

    1. Commit build files.

--- src/main/java/com/ezypay/config/WebConfig.java
+++ src/main/java/com/ezypay/config/WebConfig.java
@@ -2,10 +2,8 @@ package com.ezypay.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
 @ComponentScan(basePackages = {"com.ezypay.controller", "com.ezypay.service"})
 public class WebConfig {
 
